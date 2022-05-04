9cde5cde627500b3e2423a02f001d3742822fa5a
Lucas Garcia <lucks.garcialg@gmail.com>
2022-05-04 13:18:44 -0300

    Aula 8 - Controller Course

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { 
+	Controller, 
+	Delete, 
+	Get,
+	Param,
+	Patch,
+	Post,
+	Body,
+} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+	@Get()
+	findAll(){
+		return `Listagem de todos os Cursos`
+	}
+
+	@Get(':id')
+	findOne( @Param('id') id: string){
+		return `Curso #${id}`
+	}
+
+	@Post()
+	create(@Body() body){
+		return body
+	} 
+
+	@Patch(':id')
+	update(@Param('id') id: string, @Body() body){
+		return `Atualizacao do Curso #${id}`	
+	}
+
+	@Delete(':id')
+	remove( @Param('id') id: string){
+		return `Exclusao do Curso #${id}`
+	}
+}
